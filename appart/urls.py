7f2bd205cbc7cd40495f8a8e2d2b82ab5388c1f9
arrathilar <wervvolf@gmail.com>
2019-11-07 11:04:31 +0200

    add config of jinja2

--- appart/urls.py
+++ appart/urls.py
@@ -19,10 +19,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+
+    path('', TemplateView.as_view(template_name="main.j2")),
     
     url(r'favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
 ]
