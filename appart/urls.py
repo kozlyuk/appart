c3a76d135adcc0a9e3c0afc44e10e52cd5cf5f8d
Sergey Kozliuk <sergey.kozlyulk@gmail.com>
2019-11-08 12:20:22 +0200

    Condominium forms views urls serializers added.

--- appart/urls.py
+++ appart/urls.py
@@ -24,6 +24,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('', include('condominium.urls')),
 
     path('', TemplateView.as_view(template_name="main.j2")),
     
