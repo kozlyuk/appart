8eaffef0a6f80a26b899d9e430cc5cc3a6a642b0
Sergey Kozliuk <sergey.kozlyulk@gmail.com>
2019-11-12 09:12:26 +0200

    Accounts forms views urls serializers added.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,18 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.utils.translation import ugettext_lazy as _
 
 from accounts.models import User
+
+
+class EmailLowerField(forms.EmailField):
+    def to_python(self, value):
+        return value.lower()
+
+
+class AuthenticationForm(forms.Form):
+    email = EmailLowerField(required=True)
+    password = forms.CharField(label=_('Password'), max_length=255, required=True, widget=forms.PasswordInput)
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -9,9 +21,13 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ('email', 'mobile_number', 'birth_date', 'avatar', 'theme')
 
+    email = EmailLowerField(required=True)
+
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = User
         fields = ('email', 'mobile_number', 'birth_date', 'avatar', 'theme')
+
+    email = EmailLowerField(required=True)
