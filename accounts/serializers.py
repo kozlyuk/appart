c27f85b67cfbbba4048260ef69f033d7ac331db6
Sergey Kozliuk <sergey.kozlyulk@gmail.com>
2019-11-27 17:42:00 +0200

    AP-80 Added User pk to serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -8,6 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
+            "pk",
             "first_name",
             "last_name",
             "mobile_number",
