51cd8233b9a01269735ced281c4c1d89d97a5812
Sergey Kozliuk <sergey.kozlyulk@gmail.com>
2019-11-29 08:47:02 +0200

    AP-114 Added validation of mobile_number length

--- accounts/api.py
+++ accounts/api.py
@@ -1,6 +1,5 @@
-from rest_framework import viewsets, permissions, views
+from rest_framework import viewsets, permissions, views, status
 from rest_framework.response import Response
-
 
 from .serializers import UserSerializer
 from .models import User
@@ -22,6 +21,11 @@ class GetByNumber(views.APIView):
     permission_classes = (permissions.IsAuthenticated,)
 
     def get(self, request, mobile_number):
-        user, created = User.objects.get_or_create(mobile_number=mobile_number, defaults={'is_staff': False})
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
+        serializer = UserSerializer(data={'mobile_number': mobile_number})
+        if serializer.is_valid():
+            user, created = User.objects.get_or_create(mobile_number=mobile_number, defaults={'is_staff': False})
+            serializer = UserSerializer(user)
+            if created:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_412_PRECONDITION_FAILED)
