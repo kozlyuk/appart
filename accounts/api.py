9376f966556647ae34a499e5647a23cb1a136738
Sergey Kozliuk <sergey.kozlyulk@gmail.com>
2019-11-29 15:22:40 +0200

    AP-114 Fix validation of mobile_number length

--- accounts/api.py
+++ accounts/api.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, permissions, views, status
 from rest_framework.response import Response
+import re
 
 from .serializers import UserSerializer
 from .models import User
@@ -21,11 +22,11 @@ class GetByNumber(views.APIView):
     permission_classes = (permissions.IsAuthenticated,)
 
     def get(self, request, mobile_number):
-        serializer = UserSerializer(data={'mobile_number': mobile_number})
-        if serializer.is_valid():
-            user, created = User.objects.get_or_create(mobile_number=mobile_number, defaults={'is_staff': False})
-            serializer = UserSerializer(user)
-            if created:
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_412_PRECONDITION_FAILED)
+        message = "Mobile number must contain 10 digits"
+        if not re.match(r'^\d{10}$', mobile_number):
+            return Response(message, status=status.HTTP_412_PRECONDITION_FAILED)
+        user, created = User.objects.get_or_create(mobile_number=mobile_number, defaults={'is_staff': False})
+        serializer = UserSerializer(user)
+        if created:
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_200_OK)
