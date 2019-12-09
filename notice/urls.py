983dc76b4afbc6e15f748372ccd33a0f17de4cca
arrathilar <wervvolf@gmail.com>
2019-12-09 10:33:44 +0200

    change norice urls to lowercase

--- notice/urls.py
+++ notice/urls.py
@@ -4,16 +4,16 @@ from . import views
 
 
 urlpatterns = (
-    path("notice/Choice/", views.ChoiceListView.as_view(), name="notice_Choice_list"),
-    path("notice/Choice/create/", views.ChoiceCreateView.as_view(), name="notice_Choice_create"),
-    path("notice/Choice/detail/<int:pk>/", views.ChoiceDetailView.as_view(), name="notice_Choice_detail"),
-    path("notice/Choice/update/<int:pk>/", views.ChoiceUpdateView.as_view(), name="notice_Choice_update"),
-    path("notice/Notice/", views.NoticeListView.as_view(), name="notice_Notice_list"),
-    path("notice/Notice/create/", views.NoticeCreateView.as_view(), name="notice_Notice_create"),
-    path("notice/Notice/detail/<int:pk>/", views.NoticeDetailView.as_view(), name="notice_Notice_detail"),
-    path("notice/Notice/update/<int:pk>/", views.NoticeUpdateView.as_view(), name="notice_Notice_update"),
-    path("notice/Question/", views.QuestionListView.as_view(), name="notice_Question_list"),
-    path("notice/Question/create/", views.QuestionCreateView.as_view(), name="notice_Question_create"),
-    path("notice/Question/detail/<int:pk>/", views.QuestionDetailView.as_view(), name="notice_Question_detail"),
-    path("notice/Question/update/<int:pk>/", views.QuestionUpdateView.as_view(), name="notice_Question_update"),
+    path("notice/choice/", views.ChoiceListView.as_view(), name="notice_Choice_list"),
+    path("notice/choice/create/", views.ChoiceCreateView.as_view(), name="notice_Choice_create"),
+    path("notice/choice/detail/<int:pk>/", views.ChoiceDetailView.as_view(), name="notice_Choice_detail"),
+    path("notice/choice/update/<int:pk>/", views.ChoiceUpdateView.as_view(), name="notice_Choice_update"),
+    path("notice/notice/", views.NoticeListView.as_view(), name="notice_Notice_list"),
+    path("notice/notice/create/", views.NoticeCreateView.as_view(), name="notice_Notice_create"),
+    path("notice/notice/detail/<int:pk>/", views.NoticeDetailView.as_view(), name="notice_Notice_detail"),
+    path("notice/notice/update/<int:pk>/", views.NoticeUpdateView.as_view(), name="notice_Notice_update"),
+    path("notice/question/", views.QuestionListView.as_view(), name="notice_Question_list"),
+    path("notice/question/create/", views.QuestionCreateView.as_view(), name="notice_Question_create"),
+    path("notice/question/detail/<int:pk>/", views.QuestionDetailView.as_view(), name="notice_Question_detail"),
+    path("notice/question/update/<int:pk>/", views.QuestionUpdateView.as_view(), name="notice_Question_update"),
 )
