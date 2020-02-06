b1f3c1d8d5d3ac963aaf4fd9c7113c980357cf20
arrathilar-i3 <you@example.com>
2020-02-06 15:38:23 +0200

    AP-199 choice create view (React)

---
+++ appart-frontend/src/views/choice/choiceNew.js
@@ -0,0 +1,56 @@
+import React, {Fragment} from "react"
+import AbstractFormView from "../../generics/formViews/abstractFormView";
+import {Card, CardBody, CardHeader, Form, FormGroup, FormText, Input, Label} from "reactstrap";
+import {Text} from "react-easy-i18n";
+import {Link} from "react-router-dom";
+import Button from "reactstrap/es/Button";
+import Container from "reactstrap/es/Container";
+
+export default class ChoiceNew extends AbstractFormView{
+	constructor(props) {
+		super(props);
+		this.dataUrl = undefined
+	}
+
+	handleSubmit(){
+		console.log("new choice")
+	}
+
+	content() {
+		return (
+			<Fragment>
+				<CardHeader><Text text="choiceForm.newChoice.title" /></CardHeader>
+				<CardBody>
+					<Form>
+						<FormGroup>
+							<Label for="choiceText"><Text text="choiceForm.choiceText"/></Label>
+							<Input
+								type="textarea"
+								name="choiceText"
+							/>
+						</FormGroup>
+
+						<Link to="/choice">
+							<Button color="warning">
+								<Text text="buttons.returnBtn"/>
+							</Button>
+						</Link>
+						<Button className="float-right" onClick={this.handleSubmit}>
+							<Text text="buttons.submitBtn"/>
+						</Button>
+					</Form>
+				</CardBody>
+			</Fragment>
+		)
+	}
+
+	render() {
+		return (
+			<Container>
+				<Card>
+					{this.content()}
+				</Card>
+			</Container>
+		);
+	}
+}
