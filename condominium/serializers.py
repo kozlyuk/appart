e430643d51a292ec05f76c4eb987096d1671c9cb
kozlyuk <sergey.kozlyuk@gmail.com>
2020-02-17 17:31:23 +0200

    ApartmentSerializer update method added

--- condominium/serializers.py
+++ condominium/serializers.py
@@ -1,6 +1,10 @@
-from rest_framework import serializers
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import serializers, status
+from rest_framework.response import Response
+
 
 from condominium.models import Apartment, House, Company
+from accounts.models import User
 from accounts.serializers import UserNameSerializer
 
 
@@ -15,7 +19,7 @@ class HouseNameSerializer(serializers.ModelSerializer):
 
 
 class ApartmentSerializer(serializers.ModelSerializer):
-    house = HouseNameSerializer()
+    house = HouseNameSerializer(read_only=True)
     resident = UserNameSerializer()
 
     class Meta:
@@ -29,6 +33,22 @@ class ApartmentSerializer(serializers.ModelSerializer):
             "area",
             "residents_count",
         ]
+
+    def update(self, instance, validated_data):
+        resident_pk = validated_data.pop('resident')
+        try:
+            resident = User.objects.get(pk=resident_pk)
+            instance.resident = resident
+            instance.number = validated_data.get('number', instance.number)
+            instance.description = validated_data.get('description', instance.description)
+            instance.area = validated_data.get('area', instance.area)
+            instance.residents_count = validated_data.get('residents_count', instance.residents_count)
+            instance.save()
+        except User.DoesNotExist:
+            message = _("Resident with such pk doesn`t exists")
+            return Response(message, status=status.HTTP_404_NOT_FOUND)
+
+        return instance
 
 
 class HouseSerializer(serializers.ModelSerializer):
