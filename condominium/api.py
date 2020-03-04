3a120622a0feb2d9f9fe4689d1d2c936e7de9fc1
kozlyuk <sergey.kozlyuk@gmail.com>
2020-03-04 14:06:24 +0200

    Adding filter

--- condominium/api.py
+++ condominium/api.py
@@ -1,5 +1,7 @@
+from django.db.models import Q
 from rest_framework import viewsets
 from django_auto_prefetching import AutoPrefetchViewSetMixin
+from rest_framework.response import Response
 
 from . import serializers
 from . import models
@@ -8,8 +10,70 @@ from . import models
 class ApartmentViewSet(AutoPrefetchViewSetMixin, viewsets.ModelViewSet):
     """ViewSet for the Apartment class"""
 
-    queryset = models.Apartment.objects.all()
     serializer_class = serializers.ApartmentSerializer
+
+    def get_queryset(self):
+        queryset = models.Apartment.objects.all()
+        # get filter parameters
+        house_id=self.request.GET.get('house_id', '0')
+        is_active=self.request.GET.get('is_active', '0')
+        search_string=self.request.GET.get('filter', '').split()
+        order = self.request.GET.get('o', '0')
+
+        for word in search_string:
+            queryset = tasks.filter(Q(resident__icontains=word) |
+                                    Q(number__icontains=word) |
+                                    Q(account_number__icontains=word))
+
+        if customers != '0':
+            tasks_union = Task.objects.none()
+            for customer in customers:
+                tasks_segment = tasks.filter(deal__customer=customer)
+                tasks_union = tasks_union | tasks_segment
+            tasks = tasks_union
+        if order != '0':
+            tasks = tasks.order_by(order)
+        else:
+            tasks = tasks.order_by('-creation_date', '-deal', 'object_code')
+
+        return queryset
+
+
+#        tasks = Task.objects.all()
+        # search_string = self.request.GET.get('filter', '').split()
+        # exec_statuses = self.request.GET.getlist('exec_status', '0')
+        # owners = self.request.GET.getlist('owner', '0')
+        # customers = self.request.GET.getlist('customer', '0')
+        # order = self.request.GET.get('o', '0')
+        # for word in search_string:
+        #     tasks = tasks.filter(Q(object_code__icontains=word) |
+        #                          Q(object_address__icontains=word) |
+        #                          Q(deal__number__icontains=word) |
+        #                          Q(project_type__price_code__icontains=word) |
+        #                          Q(project_type__project_type__icontains=word))
+        # if exec_statuses != '0':
+        #     tasks_union = Task.objects.none()
+        #     for status in exec_statuses:
+        #         tasks_segment = tasks.filter(exec_status=status)
+        #         tasks_union = tasks_union | tasks_segment
+        #     tasks = tasks_union
+        # if owners != '0':
+        #     tasks_union = Task.objects.none()
+        #     for owner in owners:
+        #         tasks_segment = tasks.filter(owner=owner)
+        #         tasks_union = tasks_union | tasks_segment
+        #     tasks = tasks_union
+        # if customers != '0':
+        #     tasks_union = Task.objects.none()
+        #     for customer in customers:
+        #         tasks_segment = tasks.filter(deal__customer=customer)
+        #         tasks_union = tasks_union | tasks_segment
+        #     tasks = tasks_union
+        # if order != '0':
+        #     tasks = tasks.order_by(order)
+        # else:
+        #     tasks = tasks.order_by('-creation_date', '-deal', 'object_code')
+        # return tasks
 
 
 class HouseViewSet(viewsets.ModelViewSet):
--- payments/api.py
+++ payments/api.py
@@ -6,7 +6,7 @@ from rest_framework.serializers import ValidationError
 from rest_framework.response import Response
 from rest_framework import status
 
-from payments.serializers import BillSerializer
+from payments.serializers import BillSerializer, PaymentSerializer
 from condominium.models import Apartment
 
 
@@ -31,7 +31,7 @@ class GetTotalDebt(APIView):
         total_debt = apartment.bill_set.filter(is_active=True) \
                                        .aggregate(total_debt=Sum('total_value')) \
                                        ['total_debt'] or 0
-        return Response(total_debt, status=status.HTTP_200_OK)
+        return Response(str(total_debt), status=status.HTTP_200_OK)
 
 
 class BillListView(ListAPIView):
@@ -44,7 +44,6 @@ class BillListView(ListAPIView):
     """
 
     serializer_class = BillSerializer
-    paginate_by = 100
 
     def get_queryset(self):
         apartment_pk = self.kwargs['apartment']
@@ -53,7 +52,7 @@ class BillListView(ListAPIView):
             apartment = Apartment.objects.get(pk=apartment_pk)
         # return error HTTP_400_BAD_REQUEST if apartment does not exist
         except Apartment.DoesNotExist:
-            raise ValidationError({_('error'): [_('Apartment with such id does not exist')]})
+            raise ValidationError({'error': [_('Apartment with such id does not exist')]})
         # get bills for apartment
         queryset = apartment.bill_set.filter(is_active=True)
         queryset = self.get_serializer_class().setup_eager_loading(queryset)
@@ -65,12 +64,11 @@ class PaymentsListView(ListAPIView):
     View all payments for apartment.
 
     * Requires parameters: apartment.
-    * Only apartment owner has permission to bills.
+    * Only apartment owner has permission to payments.
     * Return error HTTP_400_BAD_REQUEST if apartment does not exist
     """
 
-    serializer_class = BillSerializer
-    paginate_by = 100
+    serializer_class = PaymentSerializer
 
     def get_queryset(self):
         apartment_pk = self.kwargs['apartment']
@@ -79,8 +77,7 @@ class PaymentsListView(ListAPIView):
             apartment = Apartment.objects.get(pk=apartment_pk)
         # return error HTTP_400_BAD_REQUEST if apartment does not exist
         except Apartment.DoesNotExist:
-            raise ValidationError({_('error'): [_('Apartment with such id does not exist')]})
-        # get bills for apartment
-        queryset = apartment.bill_set.filter(is_active=True)
-        queryset = self.get_serializer_class().setup_eager_loading(queryset)
-        return queryset.order_by('period')
+            raise ValidationError({'error': [_('Apartment with such id does not exist')]})
+        # get payments for apartment
+        queryset = apartment.payment_set.all()
+        return queryset.order_by('-date')
