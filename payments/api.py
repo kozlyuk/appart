c9cddf5487c4bcddb38a98127a0ac0d6257b97b1
kozlyuk <sergey.kozlyuk@gmail.com>
2020-04-08 15:36:00 +0300

    DO-235 change PayCallback url

--- payments/api.py
+++ payments/api.py
@@ -136,7 +136,7 @@ class PayView(APIView):
             'order_id': bill_pk,
             'version': '3',
             'sandbox': 1, # sandbox mode, set to 1 to enable it
-            'server_url': 'https://dimonline.pp.ua/payments/pay-callback/', # url to callback view
+            'server_url': 'https://dimonline.pp.ua/payments/api/v1/pay_callback/', # url to callback view
         }
         signature = liqpay.cnb_signature(params)
         data = liqpay.cnb_data(params)
@@ -158,17 +158,18 @@ class PayCallbackView(APIView):
         liqpay = LiqPay(settings.LIQPAY_PUBLIC_KEY, settings.LIQPAY_PRIVATE_KEY)
         data = request.POST.get('data')
         signature = request.POST.get('signature')
+        if not data or not signature:
+            return Response({'POST data is not provided'},
+                            status=status.HTTP_412_PRECONDITION_FAILED)
         sign = liqpay.str_to_sign(settings.LIQPAY_PRIVATE_KEY + data + settings.LIQPAY_PRIVATE_KEY)
         if sign == signature:
             response = liqpay.decode_data_from_str(data)
 
             News.objects.create(title="Success Payment", text=response)
 #            Payment.objects.create()
-            print('callback data', response)
             return Response({'check': 'callback is valid'},
                             status=status.HTTP_200_OK)
         else:
-            News.objects.create(title="Failed payment", text=response)
-            print('signature is not verified', response)
+            News.objects.create(title="Failed payment", text=data+' '+signature)
             return Response({'check': 'callback is not valid'},
                             status=status.HTTP_412_PRECONDITION_FAILED)
