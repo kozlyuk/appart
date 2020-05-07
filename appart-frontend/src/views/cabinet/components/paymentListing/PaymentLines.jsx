04c7287ade58def9f7d6b8083fc54cadebdb901a
arrathilar <wervvolf@gmail.com>
2020-05-07 12:48:22 +0300

    add sample payment

--- appart-frontend/src/views/cabinet/components/paymentListing/PaymentLines.jsx
+++ appart-frontend/src/views/cabinet/components/paymentListing/PaymentLines.jsx
@@ -6,9 +6,7 @@
  */
 
 import React from 'react';
-import { Badge, Collapse, ListGroup, ListGroupItem } from 'reactstrap';
-import { TransitionGroup } from 'react-transition-group';
-import { Flip } from 'react-reveal';
+import { Collapse } from 'reactstrap';
 import { UserConsumer } from '../../../../globalContext/userContext';
 import { Text } from 'react-easy-i18n';
 
@@ -64,24 +62,83 @@ export default class PaymentLines extends React.Component {
             <td className="text-center">{this.props.payment.description}</td>
           </tr>
           <Collapse tag="tr" isOpen={this.state.isOpen}>
-            <td colSpan={4}>
-              <TransitionGroup {...this.groupProps}>
-                {/*
-                 // @ts-ignore*/}
-                {this.props.payment.payment_service.map((service) => (
-                  <Flip top opposite cascade collapse when={this.state.isOpen}
-                        spy={this.state.isOpen}>
-                    <ListGroup>
-                      <ListGroupItem style={{ border: 'none' }}
-                                     className="justify-content-between">{service.service}
-                        <Badge color={'success'} pill className="ml-4">{service.value}</Badge>
-                      </ListGroupItem>
-                    </ListGroup>
-                  </Flip>
-                ))}
-              </TransitionGroup>
+            {/*{this.props.bill.bill_lines[0] &&*/}
+            <td style={{ padding: 0 }} colSpan={4}>
+              {/*<div>{this.props.bill.bill_lines[0].previous_debt}</div>*/}
+              <table className="table table-bordered table-hover table-sm">
+                <thead>
+                <tr>
+                  <th scope="col" style={{ width: '5%' }}><small>ТОВ "УК ДІМ"</small></th>
+                  <th scope="col" className="text-center" colSpan={10}><small>Квитанції на оплату комунальних послуг за
+                    Січень 2020
+                    р.</small></th>
+                </tr>
+                <tr>
+                  <th><small>33024, Рівненська обл., м. Рівне, вул. Макарова, 24 А</small></th>
+                  <th colSpan={5}><small>Черній Мирослав Валерійович</small></th>
+                  <th colSpan={5}><small>Особовий рахунок: 2314515</small></th>
+                </tr>
+                <tr>
+                  <th>
+                    <small>р/р уа1254135431132541 ВАТ АБ "Укргазбанк" у м. Рівне ЄДРПОУ 38416999</small>
+                  </th>
+                  <th colSpan={5}><small>Адреса: Міцкевича 130, кв.53</small></th>
+                  <th colSpan={5}><small>Площа Загальна-59,300 м. кв.</small></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <th rowSpan={2} className="text-center"><small>Вид послуг</small></th>
+                  <th rowSpan={2} className="text-center"><small>Борг на поч. місяця, грн.</small></th>
+                  <th rowSpan={2} className="text-center"><small>Оплачено, грн</small></th>
+                  <th rowSpan={2} className="text-center"><small>Субсидія, грн</small></th>
+                  <th colSpan={3} className="text-center"><small>Показники лічильника</small></th>
+                  <th rowSpan={2} className="text-center"><small>Тариф, грн</small></th>
+                  <th rowSpan={2} className="text-center"><small>Нараховано, грн</small></th>
+                  <th rowSpan={2} className="text-center"><small>Пільга, грн</small></th>
+                  <th rowSpan={2} className="text-center"><small>Сума до оплати, грн</small></th>
+                </tr>
+                <tr>
+                  <th className="text-center"><small>Останній</small></th>
+                  <th className="text-center"><small>Попередній</small></th>
+                  <th className="text-center"><small>Різниця</small></th>
+                </tr>
+                <tr>
+                  <th><small>Управління будинком</small></th>
+                  <th className="text-center"><small>210.09</small></th>
+                  <th className="text-center"><small>-</small></th>
+                  <th className="text-center"><small>-</small></th>
+                  <th className="text-center"><small>-</small></th>
+                  <th className="text-center"><small>-</small></th>
+                  <th className="text-center"><small>-</small></th>
+                  <th className="text-center"><small>3.55</small></th>
+                  <th className="text-center"><small>210.52</small></th>
+                  <th className="text-center"><small>-</small></th>
+                  <th className="text-center"><small>420.61</small></th>
+                </tr>
+                </tbody>
+              </table>
             </td>
           </Collapse>
+          {/*<Collapse tag="tr" isOpen={this.state.isOpen}>*/}
+          {/*  <td colSpan={4}>*/}
+          {/*    <TransitionGroup {...this.groupProps}>*/}
+          {/*      /!**/}
+          {/*       // @ts-ignore*!/*/}
+          {/*      {this.props.payment.payment_service.map((service) => (*/}
+          {/*        <Flip top opposite cascade collapse when={this.state.isOpen}*/}
+          {/*              spy={this.state.isOpen}>*/}
+          {/*          <ListGroup>*/}
+          {/*            <ListGroupItem style={{ border: 'none' }}*/}
+          {/*                           className="justify-content-between">{service.service}*/}
+          {/*              <Badge color={'success'} pill className="ml-4">{service.value}</Badge>*/}
+          {/*            </ListGroupItem>*/}
+          {/*          </ListGroup>*/}
+          {/*        </Flip>*/}
+          {/*      ))}*/}
+          {/*    </TransitionGroup>*/}
+          {/*  </td>*/}
+          {/*</Collapse>*/}
         </>
       );
     }
