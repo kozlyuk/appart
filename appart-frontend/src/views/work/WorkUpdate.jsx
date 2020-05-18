63630edf7a873cc1697029474529db4709a7ded4
arrathilar <wervvolf@gmail.com>
2020-05-18 12:34:57 +0300

    DO-297 Work update in dashboard

---
+++ appart-frontend/src/views/work/WorkUpdate.jsx
@@ -0,0 +1,262 @@
+/**
+ * Work update view
+ *
+ * @author          Andrey Perestyuk (Arrathilar)
+ * @email-primary   a.perestyuk@itel.rv.ua
+ * @email-secondary arrathilar@blizzard.com, a.perestyuk@archlinux.org,
+ * @copyright       2020 ITEL-Service
+ */
+
+import React, { Fragment } from 'react';
+import { Button, Card, CardBody, CardHeader, Container, Form } from 'reactstrap';
+import { Text } from 'react-easy-i18n';
+import { Link } from 'react-router-dom';
+import AbstractFormView from '../../generics/formViews/abstractFormView';
+import Page from '../../components/Page';
+import Auth from '../../auth/auth';
+import InputWithLabel from '../../components/FormInputs/InputWithLabel';
+import SelectWithChoices from '../../components/FormInputs/SelectWithChoices';
+
+export default class WorkUpdate extends AbstractFormView {
+  /**
+   * Work update constructor
+   *
+   * @param props
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      rangeValue: null,
+      // validation fields
+      password: '',
+      duration: null,
+      mobileNumber: '',
+      // defaultInactiveBtn: true,
+      errors: {
+        work: null
+      }
+    };
+    /**
+     * User object.
+     *
+     * @type {Auth}
+     * @private
+     */
+    this._user = new Auth();
+    this.dataUrl = process.env.REACT_APP_WORKS;
+    if (this.props.match) {
+      /**
+       * @type {string}
+       * @private
+       */
+      this._postUrl = process.env.REACT_APP_WORKS + this.props.match.params.id + '/';
+    }
+    this.requestType = 'put';
+    this.successRedirect = '/work';
+    /**
+     * @type {string}
+     * @private
+     */
+    this._successButton = 'Повернутися до списку робіт';
+  }
+
+  /**
+   * Collect data before submit
+   *
+   * @param target
+   * @returns {FormData}
+   */
+  submitData(target) {
+    return new FormData(document.forms.workForm);
+  }
+
+  /*
+   * Form field validation
+   * handleChange(event): void
+   *
+   * check field valid and
+   * set errors str to state
+   *
+   * @param event
+   **/
+  handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    let errors = this.state.errors;
+    this.setState({ errors, ['defaultInactiveBtn']: false });
+    switch (name) {
+      case 'name':
+        errors.name =
+          value.length < 1
+            ? [<Text text="global.validateErrors.emptyField"/>]
+            : '';
+        break;
+      case 'price_code':
+        errors.price_code =
+          value.length < 1
+            ? [<Text text="global.validateErrors.emptyField"/>]
+            : '';
+        break;
+      case 'price':
+        errors.price =
+          value.length < 1
+            ? [<Text text="global.validateErrors.emptyField"/>]
+            : '';
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ errors, [name]: value });
+  };
+
+  /**
+   * @param {string} max
+   * @param {string} min
+   * @private
+   */
+  _getTimeRangeOptionsValue(max, min) {
+    const [maxValueArr, minValueArr] = [
+      max.toString().split(':'),
+      min.toString().split(':')
+    ];
+    const halfHours = ['00', '30'];
+    let times = [];
+    for (let i = parseInt(minValueArr[0]); i <= parseInt(maxValueArr[0]); i++) {
+      if (i === parseInt(maxValueArr[0])) {
+        for (let j = parseInt(minValueArr[1]); j < 1; j++) {
+          times.push(('0' + i).slice(-2) + ':' + halfHours[j]);
+        }
+      } else {
+        for (let j = parseInt(minValueArr[1]); j < 2; j++) {
+          times.push(('0' + i).slice(-2) + ':' + halfHours[j]);
+        }
+      }
+    }
+    this.setState({ duration: times });
+  }
+
+  /**
+   * @return {*}
+   */
+  componentDidMount() {
+    this._getTimeRangeOptionsValue('8:00', '1:00');
+    return super.componentDidMount();
+  }
+
+  /**
+   * @private
+   * @param {string} date input date
+   * @param {string} parameter format parameter
+   */
+  _reformatDate(date, parameter) {
+    if (parameter === 'woSeconds') {
+      return date.substring(0, date.length - 3);
+    }
+    if (parameter === 'wSeconds') {
+      return date += ':00';
+    }
+  }
+
+  content() {
+    return (
+      <Fragment>
+        <CardHeader>{this.state.data.name}</CardHeader>
+        <CardBody>
+          <Form id="workForm" onSubmit={this.handleSubmit}>
+            <InputWithLabel
+              name={'name'}
+              label={<Text text="workForm.name"/>}
+              type={'text'}
+              error={this.state.errors.name}
+              onChange={this.handleChange}
+              defaultValue={this.state.data.name}
+            />
+            <InputWithLabel
+              name={'price_code'}
+              label={<Text text="workForm.priceCode"/>}
+              type={'text'}
+              error={this.state.errors.price_code}
+              onChange={this.handleChange}
+              defaultValue={this.state.data.price_code}
+            />
+            <InputWithLabel
+              name={'price'}
+              label={<Text text="workForm.price"/>}
+              type={'number'}
+              error={this.state.errors.price}
+              onChange={this.handleChange}
+              defaultValue={this.state.data.price}
+            />
+            <InputWithLabel
+              name={'description'}
+              label={<Text text="workForm.description"/>}
+              type={'textarea'}
+              error={this.state.errors.description}
+              onChange={this.handleChange}
+              defaultValue={this.state.data.description}
+            />
+            <SelectWithChoices
+              name={'duration'}
+              label={<Text text="workForm.duration"/>}
+              type={'select'}
+              error={this.state.errors.duration}
+              onChange={this.handleChange}
+            >
+              {this.state.duration.map(item => (
+                <option selected={this._reformatDate(this.state.data.duration, 'woSeconds') === item}
+                        key={item}
+                        value={this._reformatDate(item, 'wSeconds')}
+                >
+                  {item}
+                </option>
+              ))}
+            </SelectWithChoices>
+            <Link to="/work">
+              <Button color="warning">
+                <Text text="buttons.returnBtn"/>
+              </Button>
+            </Link>
+            <Button type="submit" color="success" onClick={this.handleSubmit} className="float-right">
+              <Text text="buttons.submitBtn"/>
+            </Button>
+          </Form>
+        </CardBody>
+      </Fragment>
+    );
+  }
+
+  /**
+   *
+   * @returns {*}
+   */
+  render() {
+    const { error, isLoaded } = this.state;
+    if (error) {
+      return <div><Text text="global.error"/>: {error.message}</div>;
+    } else if (!isLoaded) {
+      return (
+        <div className="loaderWrapper text-center mt-4">
+          <h3 className="text-center text-muted">
+            <Text text="global.loading"/>
+          </h3>
+        </div>)
+        ;
+    } else {
+
+      return (
+        <Page
+          breadcrumbs={[{ name: <Text text="sidebar.bills"/>, active: false },
+            { name: this.state.data.number, active: true }]}
+          className="TablePage"
+        >
+          <Container>
+            <Card>
+              {this.content()}
+            </Card>
+          </Container>
+        </Page>
+      );
+    }
+  }
+};
