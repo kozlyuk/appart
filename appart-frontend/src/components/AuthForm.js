61315dacc7d042d62cf69c2cf80a2c7658af0c0d
Arrathilar <Wervvolf@gmail.com>
2020-05-22 16:21:10 +0300

    add logo (#37)

--- appart-frontend/src/components/AuthForm.js
+++ appart-frontend/src/components/AuthForm.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import logo200Image from 'assets/img/logo/logo_main.png';
 import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
 import Auth from '../auth/auth';
 import FormText from 'reactstrap/lib/FormText';
@@ -130,14 +131,12 @@ class AuthForm extends React.Component {
       <Form onSubmit={this.handleSubmit}>
         {showLogo && (
           <div className="text-center pb-4">
-            <h2 className="m-auto">Дім онлайн</h2>
-            {/*<img*/}
-            {/*  src={logo200Image}*/}
-            {/*  className="rounded"*/}
-            {/*  style={{ width: 60, height: 60, cursor: 'pointer' }}*/}
-            {/*  alt="logo"*/}
-            {/*  onClick={onLogoClick}*/}
-            {/*/>*/}
+            <img
+              src={logo200Image}
+              className="rounded"
+              style={{ height: 100 }}
+              alt="logo"
+            />
           </div>
         )}
         <FormGroup>
