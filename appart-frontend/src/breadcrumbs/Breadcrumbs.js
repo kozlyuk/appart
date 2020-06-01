f21932ea380fab5a7cd6eac9b18f4520be4ef740
arrathilar <wervvolf@gmail.com>
2020-06-01 15:19:27 +0300

    i18n for breadcrumbs

--- appart-frontend/src/breadcrumbs/Breadcrumbs.js
+++ appart-frontend/src/breadcrumbs/Breadcrumbs.js
@@ -1,6 +1,44 @@
 import React from 'react';
 import withBreadcrumbs from 'react-router-breadcrumbs-hoc';
 import { Link } from 'react-router-dom';
+import { Text } from 'react-easy-i18n';
+
+const BreadcrumbsHome = () => (<Text text="sidebar.home" formatters="firstUppercase"/>);
+const BreadcrumbsUser = () => (<Text text="sidebar.user" formatters="firstUppercase"/>);
+const BreadcrumbsBill = () => (<Text text="sidebar.bills" formatters="firstUppercase"/>);
+const BreadcrumbsPayment = () => (<Text text="sidebar.payment" formatters="firstUppercase"/>);
+const BreadcrumbsHouse = () => (<Text text="sidebar.house" formatters="firstUppercase"/>);
+const BreadcrumbsApartment = () => (<Text text="sidebar.apartment" formatters="firstUppercase"/>);
+const BreadcrumbsOrder = () => (<Text text="sidebar.order" formatters="firstUppercase"/>);
+const BreadcrumbsWork = () => (<Text text="sidebar.work" formatters="firstUppercase"/>);
+const BreadcrumbsNew = () => (<Text text="breadcrumbsItems.new" formatters="firstUppercase"/>);
+const BreadcrumbsEdit = () => (<Text text="breadcrumbsItems.edit" formatters="firstUppercase"/>);
+
+const routes = [
+  { path: '/', breadcrumb: BreadcrumbsHome },
+  { path: '/user', breadcrumb: BreadcrumbsUser },
+  { path: '/user/:id/edit', breadcrumb: BreadcrumbsEdit },
+  { path: '/user/new', breadcrumb: BreadcrumbsNew },
+  { path: '/bill', breadcrumb: BreadcrumbsBill },
+  { path: '/bill/:id/edit', breadcrumb: BreadcrumbsEdit },
+  { path: '/bill/new', breadcrumb: BreadcrumbsNew },
+  { path: '/payment', breadcrumb: BreadcrumbsPayment },
+  { path: '/payment/:id/edit', breadcrumb: BreadcrumbsEdit },
+  { path: '/payment/new', breadcrumb: BreadcrumbsNew },
+  { path: '/house', breadcrumb: BreadcrumbsHouse },
+  { path: '/house/:id/edit', breadcrumb: BreadcrumbsEdit },
+  { path: '/house/new', breadcrumb: BreadcrumbsNew },
+  { path: '/apartment', breadcrumb: BreadcrumbsApartment },
+  { path: '/apartment/:id/edit', breadcrumb: BreadcrumbsEdit },
+  { path: '/apartment/new', breadcrumb: BreadcrumbsNew },
+  { path: '/order', breadcrumb: BreadcrumbsOrder },
+  { path: '/order/:id/edit', breadcrumb: BreadcrumbsEdit },
+  { path: '/order/new', breadcrumb: BreadcrumbsNew },
+  { path: '/work', breadcrumb: BreadcrumbsWork },
+  { path: '/work/:id/edit', breadcrumb: BreadcrumbsEdit },
+  { path: '/work/new', breadcrumb: BreadcrumbsNew }
+];
+
 
 /**
  * Breadcrumbs hoc for dashboard
@@ -25,4 +63,4 @@ const Breadcrumbs = ({ breadcrumbs }) => (
 );
 
 // export default withBreadcrumbs(routes)(Breadcrumbs);
-export default withBreadcrumbs()(Breadcrumbs);
+export default withBreadcrumbs(routes)(Breadcrumbs);
