0036c74c2cc5ac9c54a61a5d995272c39297e640
kozlyuk <sergey.kozlyuk@gmail.com>
2020-06-10 22:40:03 +0300

    Revert "Revert "staticfiles updated""
    This reverts commit 1790a0dd3f8dadefd3814bb90debe0f32053bc93.

--- appart/settings.py
+++ appart/settings.py
@@ -202,8 +202,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 
 # REDIS and CELERY related settings
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
