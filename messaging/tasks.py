e1b6f91712f65fdcce9964b85f977bbd2f37aa4e
kozlyuk <sergey.kozlyuk@gmail.com>
2020-06-12 11:17:02 +0300

    DO-322 Add send_sms task

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -1,9 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 
 from smtplib import SMTPException
+import requests
 from django.core.mail import EmailMessage
+from django.conf import settings
 from celery.utils.log import get_task_logger
 from appart.celery import app
+
 
 logger = get_task_logger(__name__)
 
@@ -12,10 +15,25 @@ logger = get_task_logger(__name__)
 def send_email(mail_subject, message, to):
     """ sends email to emails list """
 
-    # filter active apartments
     try:
         email = EmailMessage(mail_subject, message, to=to)
         email.send()
         logger.info("Email %s sent to %s", mail_subject, to[0])
+    except SMTPException as error:
+        logger.info("Connection error while send email to %s", error)
+
+
+@app.task
+def send_sms(recipients, message):
+    """ sends sms to recipient """
+
+    try:
+        url = f"{settings.SMS_SERVER_URL}?token={settings.SMS_TOKEN}"
+        data = {}
+        data['recipients'] = recipients
+        data['sms'] = {'sender': settings.SMS_SENDER, 'text':message}
+
+        requests.post(url, json=data)
+        logger.info("SMS %s sent to %s", message, recipients)
     except SMTPException as error:
         logger.info("Connection error while send email to %s", error)
