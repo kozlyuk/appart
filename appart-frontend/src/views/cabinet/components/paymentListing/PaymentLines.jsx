8f307bd0951f2e6641206dfc2c9bab1e109ae276
arrathilar <wervvolf@gmail.com>
2020-06-24 11:49:25 +0300

    DO-339 Fix cabinet endpoint (frontend)

--- appart-frontend/src/views/cabinet/components/paymentListing/PaymentLines.jsx
+++ appart-frontend/src/views/cabinet/components/paymentListing/PaymentLines.jsx
@@ -6,7 +6,6 @@
  */
 
 import React from 'react';
-import { Collapse } from 'reactstrap';
 import { Text } from 'react-easy-i18n';
 
 export default class PaymentLines extends React.Component {
@@ -46,70 +45,11 @@ export default class PaymentLines extends React.Component {
       return (
         <>
           <tr style={{ cursor: 'pointer' }}>
-            <td onClick={this.toggle}>{this.props.payment.payment_type}</td>
-            <td className="text-center" onClick={this.toggle}>{this.props.payment.date}</td>
-            <td className="text-center" onClick={this.toggle}>{this.props.payment.value}</td>
-            <td className="text-center" onClick={this.toggle}>{this.props.payment.description}</td>
+            <td>{this.props.payment.payment_type}</td>
+            <td className="text-center">{this.props.payment.date}</td>
+            <td className="text-center">{this.props.payment.value}</td>
+            <td className="text-center">{this.props.payment.description}</td>
           </tr>
-          <Collapse tag="tr" isOpen={this.state.isOpen}>
-            {/*{this.props.bill.bill_lines[0] &&*/}
-            <td style={{ padding: 0 }} colSpan={4}>
-              {/*<div>{this.props.bill.bill_lines[0].previous_debt}</div>*/}
-              <table className="table table-bordered table-hover table-sm">
-                <thead>
-                <tr>
-                  <th scope="col" style={{ width: '5%' }}><small>ТОВ "УК ДІМ"</small></th>
-                  <th scope="col" className="text-center" colSpan={10}><small>Квитанції на оплату комунальних послуг за
-                    Січень 2020
-                    р.</small></th>
-                </tr>
-                <tr>
-                  <th><small>33024, Рівненська обл., м. Рівне, вул. Макарова, 24 А</small></th>
-                  <th colSpan={5}><small>Черній Мирослав Валерійович</small></th>
-                  <th colSpan={5}><small>Особовий рахунок: 2314515</small></th>
-                </tr>
-                <tr>
-                  <th>
-                    <small>р/р уа1254135431132541 ВАТ АБ "Укргазбанк" у м. Рівне ЄДРПОУ 38416999</small>
-                  </th>
-                  <th colSpan={5}><small>Адреса: Міцкевича 130, кв.53</small></th>
-                  <th colSpan={5}><small>Площа Загальна-59,300 м. кв.</small></th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                  <th rowSpan={2} className="text-center"><small>Вид послуг</small></th>
-                  <th rowSpan={2} className="text-center"><small>Борг на поч. місяця, грн.</small></th>
-                  <th rowSpan={2} className="text-center"><small>Оплачено, грн</small></th>
-                  <th rowSpan={2} className="text-center"><small>Субсидія, грн</small></th>
-                  <th colSpan={3} className="text-center"><small>Показники лічильника</small></th>
-                  <th rowSpan={2} className="text-center"><small>Тариф, грн</small></th>
-                  <th rowSpan={2} className="text-center"><small>Нараховано, грн</small></th>
-                  <th rowSpan={2} className="text-center"><small>Пільга, грн</small></th>
-                  <th rowSpan={2} className="text-center"><small>Сума до оплати, грн</small></th>
-                </tr>
-                <tr>
-                  <th className="text-center"><small>Останній</small></th>
-                  <th className="text-center"><small>Попередній</small></th>
-                  <th className="text-center"><small>Різниця</small></th>
-                </tr>
-                <tr>
-                  <th><small>Управління будинком</small></th>
-                  <th className="text-center"><small>210.09</small></th>
-                  <th className="text-center"><small>-</small></th>
-                  <th className="text-center"><small>-</small></th>
-                  <th className="text-center"><small>-</small></th>
-                  <th className="text-center"><small>-</small></th>
-                  <th className="text-center"><small>-</small></th>
-                  <th className="text-center"><small>3.55</small></th>
-                  <th className="text-center"><small>210.52</small></th>
-                  <th className="text-center"><small>-</small></th>
-                  <th className="text-center"><small>420.61</small></th>
-                </tr>
-                </tbody>
-              </table>
-            </td>
-          </Collapse>
           {/*<Collapse tag="tr" isOpen={this.state.isOpen}>*/}
           {/*  <td colSpan={4}>*/}
           {/*    <TransitionGroup {...this.groupProps}>*/}
