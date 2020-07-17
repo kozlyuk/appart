5da3058dcd4072ddc249c1b2e756cc285e3d7fc1
kozlyuk <sergey.kozlyuk@gmail.com>
2020-07-17 10:52:01 +0300

    DO-382 Changing password from dashboard

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,11 @@
-from django.apps import AppConfig
+from django.apps import AppConfig, apps
+from django_rest_passwordreset.signals import reset_password_token_created
+from accounts.signals import password_reset_token_created
 
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+    # def ready(self):
+    #     sender = apps.get_model(app_label='accounts', model_name='User')
+    #     reset_password_token_created.connect(password_reset_token_created, sender=sender)
