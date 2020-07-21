2e2ee8d4440e582eae4dbf7ae9e422e915324925
arrathilar <wervvolf@gmail.com>
2020-07-21 16:35:44 +0300

    DO-402 add reset confirmation form

--- appart-frontend/src/NonAuthRoutes.js
+++ appart-frontend/src/NonAuthRoutes.js
@@ -13,6 +13,7 @@ import { STATE_LOGIN, STATE_SIGNUP } from './components/AuthForm';
 import RegistrationForm from './views/registration/RegistrationForm';
 import { Redirect } from 'react-router';
 import Activation from './views/registration/Activation';
+import ResetActivation from './views/registration/ResetActivation';
 
 export default class NonAuthRoutes extends React.Component {
   constructor(props) {
@@ -36,6 +37,14 @@ export default class NonAuthRoutes extends React.Component {
           layout={EmptyLayout}
           component={props => (
             <RegistrationForm {...props} authState={STATE_SIGNUP}/>
+          )}
+        />
+        <LayoutRoute
+          exact
+          path="/registration/password_reset/:token"
+          layout={EmptyLayout}
+          component={props => (
+            <ResetActivation {...props}/>
           )}
         />
         <LayoutRoute
